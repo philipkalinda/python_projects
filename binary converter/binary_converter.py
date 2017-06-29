0688bb0fe70ba54f4af0b75008bf9e912ef7888e
Philip Kalinda <philipkalinda@gmail.com>
2017-06-29 11:50:47 +0100

    binary_converter object fix ~ self

--- binary converter/binary_converter.py
+++ binary converter/binary_converter.py
@@ -1,23 +1,28 @@
 # Binary Converter
+import string
 class BinConverter():
     # Dictionaries
-    import string
-    letter_dict = {each_print: bin(ord(each_print) + 256)[2:] for each_print in string.printable}
-    binary_dict = {value: key for key, value in letter_dict.items()}
+#     import string
+#     letter_dict = {each_print: bin(ord(each_print) + 256)[2:] for each_print in string.printable}
+#     binary_dict = {value: key for key, value in letter_dict.items()}
+    
+    def __init__(self):
+        self.letter_dict = {each_print: bin(ord(each_print) + 256)[2:] for each_print in string.printable}
+        self.binary_dict = {value: key for key, value in letter_dict.items()}
 
-    def dictionary():
+    def dictionary(self):
         """This displays the dictionay used to converts string inputs into binary outputs
         """
-        for key, value in BinConverter.letter_dict.items():
+        for key, value in self.letter_dict.items():
             print('{} -> {}'.format(key, value))
 
-    def encrypt(message):
+    def encrypt(self, message):
         """This converts an alpha-numeric string into a binary output based on the binary dictionary in the script
         """
-        bin_temp = [BinConverter.letter_dict[each_letter] for each_letter in message]
+        bin_temp = [self.letter_dict[each_letter] for each_letter in message]
         return ''.join(bin_temp)
 
-    def decrypt(message):
+    def decrypt(self, message):
         """This converts a binary output into the alpha-numeric string equivalent based on the binary dictionary in the script
         """
         char_count = int(len(message) / 9)
@@ -25,7 +30,7 @@ class BinConverter():
         binary_length = 9
         temp = []
         for _ in range(1, char_count + 1):
-            temp.append(BinConverter.binary_dict[message[((current_count * binary_length) - binary_length):(current_count * binary_length)]])
+            temp.append(self.binary_dict[message[((current_count * binary_length) - binary_length):(current_count * binary_length)]])
             current_count += 1
         return ''.join(temp)
 
@@ -36,11 +41,11 @@ class BinConverter():
         confirm = (input('''Would you like to:\n1. Convert text to binary (enter -> 1 or \'text\') or \n2. Convert binary to text (enter -> 2 or \'binary\')\n> ''')).lower()
         if confirm == '1' or confirm == 'text' or confirm == 't' or confirm[0] == 't':
             message = input('Please enter the text message you would like to convert to binary?\n> ')
-            print(BinConverter.encrypt(message))
+            print(self.encrypt(message))
         elif confirm == '2' or confirm == 'binary' or confirm == 'b' or confirm[0] == 'b':
             message = input('Please enter the binary message you would like to convert to text\n> ')
-            print(BinConverter.decrypt(message))
+            print(self.decrypt(message))
         else:
             print('*********************************************\nPlease enter:\n1 to convert to binary\tor\n2 to convert to text')
-            BinConverter.convert()
+            self.convert()
 
